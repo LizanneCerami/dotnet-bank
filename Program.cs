b6a1ae4d73e842a6abd86f054ebcfffe4d414a90
Lizanne Cerami <lizanne.cerami@gmail.com>
2023-05-09 16:37:38 -0400

    made a bank account

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿
 // Console.WriteLine("Hello, World!");
 
+/*
 int age = 34;
 string name = "Liz";
 bool isHere = true;
@@ -8,4 +9,14 @@ float salary = 22000.01f;
 
 Console.WriteLine($"Hi, {name}! Your age is {age} and is here? {isHere}");
 Console.WriteLine($"And you make ${salary} a year");
+*/
 
+try
+{
+  savingsAccount myAccount = new savingsAccount(300);
+  // myAccount.balance = 300;
+
+  Console.WriteLine($"Your balance is ${myAccount.getBalance()}");
+} catch (Exception ex) {
+  Console.WriteLine($"We got an error: {ex.Message}");
+}
