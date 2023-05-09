5dd800da5924b03ba4c563c3cf6fe1fb0ce600ba
Lizanne Cerami <lizanne.cerami@gmail.com>
2023-05-09 14:40:24 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿
+// Console.WriteLine("Hello, World!");
+
+int age = 34;
+string name = "Liz";
+bool isHere = true;
+float salary = 22000.01f;
+
+Console.WriteLine($"Hi, {name}! Your age is {age} and is here? {isHere}");
+Console.WriteLine($"And you make ${salary} a year");
+
